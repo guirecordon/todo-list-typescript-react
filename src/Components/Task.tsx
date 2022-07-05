3cf957f35279c7755d901c658dbcf464ac67ef89
Gui Recordon <guilherme.recordon@gmail.com>
2022-07-05 16:27:38 -0300

    Add checked off styling

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,12 +1,26 @@
-import { Trash } from 'phosphor-react'
+import { Trash, Check } from 'phosphor-react'
+import { useState } from 'react';
 
 import styles from './Task.module.css'
 
 export function Task() {
+  const [check, setCheck] = useState(false);
+
+  function handleCheckOff() {
+    setCheck(!check);
+    console.log(check);
+    
+  }
+
+
   return (
     <div className={styles.task}>
-      <div className={styles.check}></div>
-      <p className={styles.content}>This is my first task</p>
+      <div 
+        onClick={handleCheckOff} 
+        className={`${styles.check} ${check && styles.checkedOff}`}>
+          {check && <Check className={styles.icon} size={16} weight="bold" />}
+      </div>
+      <p className={`${styles.content} ${check && styles.lineThrough}`}>This is my first task</p>
       <Trash size={24} color={'var(--gray-300)'} />
     </div>
   )
