5cb8434632cd8be00328f8d1e630a556cf7dc15b
Gui Recordon <guilherme.recordon@gmail.com>
2022-07-05 11:51:23 -0300

    add empty task board section

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -1,8 +1,36 @@
+import {ClipboardText} from 'phosphor-react'
+import styles from './List.module.css'
+
 export function List() {
   return (
-    <section>
-      <header>List header</header>
-      List
-    </section>
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <div className={`${styles.criadas} ${styles.counterContainer}`}>
+          Tarefas criadas
+          <span className={styles.counter}>0</span>
+        </div>
+        <div className={`${styles.concluidas} ${styles.counterContainer}`}>
+          Concluídas
+          <span className={styles.counter}>0</span>
+        </div>
+      </header>
+      <div>
+        <section className={styles.emptyBoard}>
+            <div className={styles.boardMessage}>
+              <ClipboardText size={56} color="var(--gray-400)" />
+              <div className={styles.paragraphs}>
+                <p 
+                  className={styles.topLine}
+                >
+                  Você ainda não tem tarefas cadastradas
+                </p>
+                <p>
+                  Crie tarefas e organize seus itens a fazer
+                </p>
+              </div>
+            </div>
+          </section>
+      </div>
+    </div>
   )
 }
